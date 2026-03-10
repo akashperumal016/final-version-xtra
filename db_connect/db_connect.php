6dd1885099c0cd8f5164f388e9909211412f736c
akashperumal016 <akashperumal016@gmail.com>
2026-03-10 23:41:15 +0530

    Refactor db_connect to use getenv for credentials
    Updated database connection variables to use getenv() for improved security.

--- db_connect/db_connect.php
+++ db_connect/db_connect.php
@@ -1,12 +1,16 @@
 <?php
 
-$host = ${DB_HOST};
-$username = ${DB_USERNAME}
-$password = ${DB_PASSWORD}
-$database = ${DB_NAME}
+$host = getenv("DB_HOST");
+$username = getenv("DB_USERNAME");
+$password = getenv("DB_PASSWORD");
+$database = getenv("DB_NAME");
 
 $conn = mysqli_connect($host, $username, $password, $database);
 
 if (!$conn) {
     die("Connection Failed: " . mysqli_connect_error());
 }
+
+echo "Connected successfully";
+
+?>
